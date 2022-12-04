430dc7a17da3511448c5f7b94564dab6b63a4c89
StefAlfin <119769081+StefAlfin@users.noreply.github.com>
2022-12-04 09:09:57 +0700

    Add files via upload

---
+++ storage/framework/views/a02d0198b7a627196ecc6eca9e6e24ad3f882296.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+</head>
+<body>
+    <button class="btn btn-info">Tes Button</button>
+</body>
+</html><?php /**PATH C:\Users\stefa\Desktop\task-5-fullstack\resources\views/welcome.blade.php ENDPATH**/ ?>
